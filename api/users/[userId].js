a94fa8662daba7f25ffe954fbc0051acaab22334
SansYT <128694801+SansYT789@users.noreply.github.com>
2026-01-15 20:57:29 +0700

    Update [userId].js

--- api/users/[userId].js
+++ api/users/[userId].js
@@ -1,1 +1,35 @@
+import { db } from '../../lib/database.js';
+import { setCORS, sendJSON, sendError, requireAppAuth } from '../../lib/helpers.js';
 
+export default async function handler(req, res) {
+  setCORS(res);
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (!requireAppAuth(req, res)) return;
+
+  const { userId } = req.query;
+
+  // GET - Retrieve user
+  if (req.method === 'GET') {
+    const { field } = req.query;
+    const user = await db.findById('users', userId);
+
+    if (!user) {
+      return sendError(res, 'User not found', 404);
+    }
+
+    if (field) {
+      if (!(field in user)) {
+        return sendError(res, 'Field not found', 404);
+      }
+      return sendJSON(res, { [field]: user[field] });
+    }
+
+    return sendJSON(res, user);
+  }
+
+  return sendError(res, 'Method not allowed', 405);
+}
